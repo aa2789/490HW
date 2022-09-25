b00990ff25f3e76123fc20daa15ab5a3b68911b5
abhiarun <aa2789@njit.edu>
2022-09-25 19:06:03 -0400

    final changes

--- HW1/EX1.java
+++ HW1/EX1.java
@@ -1,23 +1,20 @@
 class EX1{
     public static void main(String[] args){
         ConfigParameters obj = ConfigParameters.Instance();
-        //check if default values are correct
-        System.out.println(obj.getFilename());
-        System.out.println(obj.getNumberOfUsers());
-        System.out.println(obj.soundIsOn());
-        //tests to demonstrate that the parameters are set and retrieved correctly.
-        obj.setSoundOn();
+        System.out.println("Initial Values:");
+        System.out.println("Filename = " + obj.getFilename());
+        System.out.println("Number of Users = " + obj.getNumberOfUsers());
+        System.out.println("Sound = "+ obj.soundIsOn());
+        System.out.println();
         obj.setFilename("DFile.dat");
         obj.setNumberOfUsers(57);
-        System.out.println(obj.getFilename());
-        System.out.println(obj.getNumberOfUsers());
-        System.out.println(obj.soundIsOn());
-        ConfigParameters obj2 = ConfigParameters.Instance();
-        System.out.println(obj2.getFilename());
-        System.out.println(obj2.getNumberOfUsers());
-        System.out.println(obj2.soundIsOn());
- 
-        
-        
+        obj.setSoundOn();
+        System.out.println("Values after changed to Filename = DFile.dat, ");
+        System.out.println("                        Number of Users = 57, ");
+        System.out.println("                        and Sound = true: ");
+        System.out.println("Filename = " + obj.getFilename());
+        System.out.println("Number of Users = " + obj.getNumberOfUsers());
+        System.out.println("Sound = "+ obj.soundIsOn());
+  
     }
 }
